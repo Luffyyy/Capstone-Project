77ba871e3b7b4995fa04031ba144bc8015ec4edf
Luffy <luffydafloffi@gmail.com>
2026-05-04 00:50:45 +0300

    Fix decrypt function

--- Project/Assets/CET/Scripts/VPL/Functions/DecryptFunction.cs
+++ Project/Assets/CET/Scripts/VPL/Functions/DecryptFunction.cs
@@ -4,23 +4,23 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Decrypt", menuName = "VPL/Functions/Decrypt")]
-public class DecryptFunction : FuncBlockDefinition
+[CreateAssetMenu(fileName = "DecryptFunction", menuName = "VPL/Functions/Decrypt")]
+public class DecryptFunction : FuncExpressionBlockDefinition
 {
     public override List<VPLArg> Args => new()
     {
         new() { Name = "char", Type = "str" },
+        new() { Name = "key", Type = "num" },
     };
 
     public override object ExecuteWithReturn(params object[] input)
     {
         char c = ((string)input[0])[0];
+        int k = int.Parse(input[1].ToString());
         if (c < 'a' || c > 'z') return null; // Ignore characters that aren't between a-z
 
         c -= 'a';
 
-        Debug.Log((c-6) % 26);
-
-        return ((char)((c-6) % 26 + 'a')).ToString();
+        return ((char)((c-k) % 26 + 'a')).ToString();
     }
 }
