f21430a2644bcad528e7a3dab8d11305aac48900
EliasFarah2 <eliasfarah776@gmail.com>
2026-06-09 14:57:15 +0300

    fixed notes bug played only on server
    notes were being played only on server side but when a player interacted with the model no sound was played. also fixed the swap note function that  was not making  any sound on dedicated server mode

--- Project/Assets/CET/Scripts/Entities/Door.cs
+++ Project/Assets/CET/Scripts/Entities/Door.cs
@@ -18,8 +18,10 @@ public class Door : Activatable
     protected override void OnIsOnChanged(bool oldValue, bool newValue)
     {
         base.OnIsOnChanged(oldValue, newValue);
+        Debug.Log($"Door hook {oldValue}->{newValue}");
         if (!IsOpenTriggered) // This means the door is open the moment we turn on the object
         {
+            Debug.Log("Setting animator isOpen = " + newValue);
             animator.SetBool("isOpen", newValue);
         }
         if (IsLastDoor && newValue)
